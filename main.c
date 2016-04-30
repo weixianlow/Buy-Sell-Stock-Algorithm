c78ea34d16269b4a2e9b5f11aa0d814cdbd7d760
Wei Xian Low <weix.low@gmail.com>
2016-04-30 00:35:44 -0500

    need more checking -x

--- main.c
+++ main.c
@@ -18,14 +18,19 @@
 #include "error.h"
 #include "vector.h"
 
-void parsing(struct vector* v, char*);
-void stockBuySell(struct vector* v, int number_days, int r);
-
 typedef struct set
 {
-    int toBuy;
-    int toSell;
+    int toBuyHere;
+    int toSellHere;
 }buySellSet;
+
+void parsing(int*, char*, int);
+void stockBuySell(int* prices, int number_days, int r);
+void stockBuySell2(int* prices, int);
+void findGlobalParent(int* globalDirection[], int* localDirection[], int[], int[], int counter, int i, int j, int* code );
+void findLocalParent(int* globalDirection[], int* localDirection[], int[], int[], int counter, int i, int j, int* code);
+
+
 /*
  * 
  */
@@ -36,35 +41,39 @@ int main(int argc, char** argv) {
         exit(INCORRECT_NUMBER_OF_COMMAND_LINE_ARGUMENTS);
     }
     
-    struct vector v;
-    parsing(&v, argv[3]);
+    
+    
     
     int i;
     
     int numberOfDays = atoi(argv[1]);
     int r = atoi(argv[2]);
     
-    if(v.size != numberOfDays){
-        exit(INCORRECT_NUMBER_OF_DAYS_INPUTTED);
-    }
+    int prices[numberOfDays];
+    parsing(prices, argv[3], numberOfDays);
     
-//    for (i=0;i<v.size;i++)
+    
+    
+    
+    
+//    for (i=0;i<numberOfDays;i++)
 //    {
-//        printf("\nReading value at index %d: %d", i, v.data[i]-48);
+//        printf("\nReading value at index %d: %d", i, prices[i]);
 //    }
     
-    stockBuySell(&v, numberOfDays, r);
-    free_vector(&v);
+    
+    stockBuySell(prices, numberOfDays, r);
+    
     
     return (EXIT_SUCCESS);
 }
 
-void parsing(struct vector* v, char* fileInput){
+void parsing(int* prices, char* fileInput, int numberOfDays){
     
     FILE* fp = fopen(fileInput, "r");
     char c;
     int input;
-    int count;
+    int count=0;
    
 //    struct vector_value value;
     
@@ -72,30 +81,9 @@ void parsing(struct vector* v, char* fileInput){
         exit(FILE_FAILED_TO_OPEN);
     }
     
-    init_vector(v);
     
-//    while( (c=getc(fp))!= EOF){
-//        if(c == '\n'){
-//            input = get_value_and_clear_vector(&value);
-//            insert_element_vector(v, input);
-//            printf("Added: %c\n", input);
-//            
-//            continue;
-//        }
-//        if(isdigit(c)){
-//            insert_element_vector_value(&value, c);
-//        }else{
-//            free_vector(v);
-//            exit(PARSING_ERROR_INVALID_CHARACTER_ENCOUNTERED);
-//        }
-//        
-//    }
     
-//    if(feof(fp)){
-//       fclose(fp);
-//        free_vector(v);
-//        exit(PARSING_ERROR_EMPTY_FILE);
-//    }
+
     
     while(!feof(fp)){
         //count++;
@@ -103,22 +91,22 @@ void parsing(struct vector* v, char* fileInput){
         
     if(!fscanf(fp, "%d\n", &input)){
         fclose(fp);
-        free_vector(v);
+        
         exit(PARSING_ERROR_INVALID_CHARACTER_ENCOUNTERED);
     }
     else{
-        insert_element_vector(v, input);
-        //printf("Added: %d\n", input);
+        prices[count] = input;
+        count++;
     }
     
     
-    }
     
-    if((*v).size == 0){
+    }
+    if(count != numberOfDays){
         fclose(fp);
-        free_vector(v);
-        exit(PARSING_ERROR_EMPTY_FILE);
+        exit(INCORRECT_NUMBER_OF_DAYS_INPUTTED);
     }
+    
     
 //    if(count == 0){
 //        fclose(fp);
@@ -127,87 +115,165 @@ void parsing(struct vector* v, char* fileInput){
 //    }
 }
 
-void stockBuySell(struct vector* v, int number_days, int r)
+void stockBuySell(int* array, int number_days, int r)
 {
-    // Prices must be given for at least two days
-    if (number_days == 1)
-        return;
-
-    if(r == 1){
-        
-        int maximum = access_element_vector(v, 0);
-        int minimum = access_element_vector(v, 0);
-        
-        int i;
-        
-        for(i=0;i<number_days;i++)
-        {
-            if(access_element_vector(v, i) < minimum)
-            {
-                minimum = access_element_vector(v, i);
-            }
-            
-            if(access_element_vector(v, i) > maximum)
-            {
-                maximum = access_element_vector(v, i);
-            }
-        }
-        
-        printf("%d\n", minimum);
-        printf("%d\n", maximum);
-        
-        return;
-        
-    }else{
-        
-    int count = 0; 
-
-    // solution vector
-    buySellSet product[number_days/2 + 1];
-
     
-    int i = 0;
-    while (i < number_days-1)
-    {
-        
-        while ((i < number_days-1) && (access_element_vector(v, i+1) <= access_element_vector(v, i))){
-            i++;
-        }
-
-   
-        if (i == number_days-1)
-        {
-            break;
-        }
-      
-        product[count].toBuy = i++;
-
-
-        while ((i < number_days) && (access_element_vector(v, i) >= access_element_vector(v, i-1))){
-           i++; 
-        }
-            
-
-     
-        product[count].toSell = i-1;
-
-        
-        count++;
-    }
-
-
-    if (count == 0){
+    if(number_days < 2){
         return;
     }
-    else
-    {
-   
-       for (int i = 0; i < count; i++){
-          printf("%d\n", product[i].toBuy+1);
-          printf("%d\n", product[i].toSell+1);
-       }
+    
+    int days = number_days;
+    if(r >= days){
+        return stockBuySell2(array, number_days);
     }
+    int i=0, j=0;
+    int local[days][r+1];
+    int global[days][r+1];
+    int* localDirection[days];
+    int* globalDirection[days];
+    int code =1;
+    int buy[r];
+    int sell[r];
+    
+    
+    
+//    buySellSet setofBuySell[r];
+    int counter = r-1;
+    
+    
+    for(i=0;i<r;i++){
+       buy[i] = -1;
+       sell[i] = -1;
+    }
+    
+    for(i=0;i<days;i++){
+        localDirection[i] = malloc(sizeof(int)*(r+1));
+        globalDirection[i] = malloc(sizeof(int)*(r+1));
+    }
+    
+    for(i=0;i<days;i++){
+        for(j=0;j<r+1;j++){
+            localDirection[i][j] = 0;
+            globalDirection[i][j] = 0;
+        }
+    }
+    
+    
+    for(i=0;i<days;i++){
+        for(j=0;j<r+1;j++){
+            local[i][j] = 0;
+            
+            global[i][j] = 0;
+            
+            
+            
+                    
+        }
+    }
+    for( i=1;i<days;i++){
+        int difference = array[i] - array[i-1];
+        
+        for( j=1;j<=r;j++){
+            if(global[i-1][j-1] >= local[i-1][j]+ difference){
+                local[i][j] = global[i-1][j-1];
+                localDirection[i][j] = 2;
+            }else{
+                local[i][j] = local[i-1][j]+ difference;
+                localDirection[i][j] = 1;
+                
+            }
+            
+            if(global[i - 1][j] >= local[i][j]){
+                global[i][j] = global[i - 1][j];
+                globalDirection[i][j] = 3;
+            }else{
+                global[i][j] = local[i][j];
+                globalDirection[i][j] = 4;
+                
+            }
+        }
+    }
+//    printf("Local Direction\n");
+//    for(i=0;i<r+1;i++){
+//        printf("%d %d %d %d %d %d\n", localDirection[0][i], localDirection[1][i], localDirection[2][i], localDirection[3][i], localDirection[4][i], localDirection[5][i]);
+//    }
+//    printf("Global Direction\n");
+//    for(i=0;i<r+1;i++){
+//        printf("%d ", globalDirection[0][i]);
+//        printf("%d ", globalDirection[1][i]);
+//        printf("%d ", globalDirection[2][i]);
+//        printf("%d ", globalDirection[3][i]);
+//        printf("%d ", globalDirection[4][i]);
+//        printf("%d ", globalDirection[5][i]);
+//        printf("\n");
+//    }
+    findGlobalParent(globalDirection, localDirection, buy, sell, counter, days-1, r, &code);
+    
+    for(i=0;i<r;i++){
+        if(buy[i] == -1){
+            
+        }else{
+            printf("%d\n", buy[i]+1);
+        }
+        if(sell[i] == -1){
+            
+        }else{
+            printf("%d\n", sell[i]+1);
+        }
+    }
+    
+    //printf("\nProfit is %d", global[days-1][r]);
+    
+    
+}
 
-    return;
+void stockBuySell2 (int array[], int numberOfDays){
+    int maxProfit =0;
+    
+    for(int i=1;i<numberOfDays;i++){
+        if(array[i] > array[i-1]){
+            maxProfit += array[i] - array[i-1];
+        }
+    }
+    
+    printf("Maximum profit from stockBuySell2 : %d", maxProfit);
+}
+
+void findGlobalParent(int* globalDirection[], int* localDirection[], int buy[], int sell[], int counter, int i, int j, int* code){
+    
+    
+    while(*code){
+        if ((globalDirection[i][j]) == 0){
+            *code = 0;
+            break;
+        }else{
+            if((globalDirection[i][j]) == 4){
+                sell[counter] = i;
+                findLocalParent(globalDirection, localDirection, buy, sell, counter, i, j, code);
+            }else
+            
+            if((globalDirection[i][j]) == 3){
+                i--;
+            }
+        }
     }
 }
+
+void findLocalParent(int* globalDirection[], int* localDirection[], int buy[], int sell[], int counter, int i, int j, int* code){
+    
+    while(*code){
+        if(localDirection[i][j] == 0){
+            buy[counter] = i;
+            *code = 0;
+            break;
+        }else{
+            if(localDirection[i][j] == 2){
+                buy[counter--] = i;
+                findGlobalParent(globalDirection, localDirection, buy, sell, counter, i-1, j, code);
+            }else if(localDirection[i][j] == 1){
+                i--;
+            }
+        }
+    }
+}
+
